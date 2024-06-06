9af30d023ed8fdf5adc52acebae92b9cf77c19ef
milos-jolovic <mj20200179@student.fon.bg.ac.rs>
2024-06-06 16:34:42 +0200

    Eighth commit implemented Spring security with Bearer tokens.

--- src/main/java/rs/ac/bg/fon/ai/naprednoProgramiranje/NaprednoProgramiranjeApplication.java
+++ src/main/java/rs/ac/bg/fon/ai/naprednoProgramiranje/NaprednoProgramiranjeApplication.java
@@ -2,7 +2,10 @@ package rs.ac.bg.fon.ai.naprednoProgramiranje;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import rs.ac.bg.fon.ai.naprednoProgramiranje.config.RsaKeyProperties;
 
+@EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 public class NaprednoProgramiranjeApplication {
 
